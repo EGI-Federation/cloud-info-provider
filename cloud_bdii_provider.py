e61b2555422085ee2fb669873725336d84509a26
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-05-14 13:43:44 +0200

    Try to indent properly the defitions

--- cloud_bdii_provider.py
+++ cloud_bdii_provider.py
@@ -9,67 +9,150 @@
 ##
 ## Interfaces info
 ##
-interface = {}
-interface['IaaS_api']='OCCI'
-interface['IaaS_api_version']='1.1'
-interface['IaaS_api_endpoint_technology']='REST'
-interface['IaaS_api_authorization_method']='X509-VOMS'
-interface['STaaS_api']='CDMI'
-interface['STaaS_api_version']='1.0.1'
-interface['STaaS_api_endpoint_technology']='REST'
-interface['STaaS_api_authorization_method']='X509-VOMS'
+interface = {
+    'IaaS_api': 'OCCI',
+    'IaaS_api_version': '1.1',
+    'IaaS_api_endpoint_technology': 'REST',
+    'IaaS_api_authorization_method': 'X509-VOMS',
+    'STaaS_api': 'CDMI',
+    'STaaS_api_version': '1.0.1',
+    'STaaS_api_endpoint_technology': 'REST',
+    'STaaS_api_authorization_method': 'X509-VOMS',
+}
 
 ##
 ## Site INFOS
 ##
-provider = {}
-provider['site_name']    = 'PRISMA-INFN-BARI'
-provider['www']          = 'http://recas-pon.ba.infn.it/'
-provider['country']      = 'IT'
-provider['site_longitude'] = '16.88';
-provider['site_latitude'] = '41.11';
-provider['affiliated_ngi'] = 'NGI_IT'
-provider['user_support_contact'] = 'prisma-iaas-open-support@lists.ba.infn.it'
-provider['general_contact'] = 'prisma-iaas-open-support@lists.ba.infn.it'
-provider['sysadmin_contact'] = 'prisma-iaas-open-support@lists.ba.infn.it'
-provider['security_contact'] = 'prisma-iaas-open-support@lists.ba.infn.it'
-provider['production_level'] = 'production' # e.g. 'production'
-provider['site_bdii_host'] = 'prisma-cloud.ba.infn.it'
-provider['site_bdii_port'] = '2170'
+provider = {
+    'site_name': 'PRISMA-INFN-BARI',
+    'www': 'http://recas-pon.ba.infn.it/',
+    'country': 'IT',
+    'site_longitude': '16.88',
+    'site_latitude': '41.11',
+    'affiliated_ngi': 'NGI_IT',
+    'user_support_contact': 'prisma-iaas-open-support@lists.ba.infn.it',
+    'general_contact': 'prisma-iaas-open-support@lists.ba.infn.it',
+    'sysadmin_contact': 'prisma-iaas-open-support@lists.ba.infn.it',
+    'security_contact': 'prisma-iaas-open-support@lists.ba.infn.it',
+    'production_level': 'production', # e.g. 'production'
+    'site_bdii_host': 'prisma-cloud.ba.infn.it',
+    'site_bdii_port': '2170',
 
 
-#Site resources information
-provider['site_total_cpu_cores']   = '300'
-provider['site_total_ram_gb']   = '600'
-provider['site_total_storage_gb']   = '51200'
+    'site_total_cpu_cores': '300',
+    'site_total_ram_gb': '600',
+    'site_total_storage_gb': '51200',
 
-#Infrastructure-as-a-Service
-provider['iaas_middleware'] = 'OpenStack Nova' #e.g. OpenNebula
-provider['iaas_middleware_version'] = 'havana' #CLOUD_MW version, e.g. 3.0
-provider['iaas_middleware_developer'] = 'OpenStack' #CLOUD_MW version, e.g. 3.0
-provider['iaas_hypervisor'] = 'KVM' #e.g. OpenNebula
-provider['iaas_hypervisor_version'] = '1.5.0' #e.g. OpenNebula
-provider['iaas_capabilities'] = ('cloud.managementSystem','cloud.vm.uploadImage') # Capabilities must be agreed with the FedClouds task force
+    'iaas_middleware': 'OpenStack Nova',
+    'iaas_middleware_version': 'havana',
+    'iaas_middleware_developer': 'OpenStack',
+    'iaas_hypervisor': 'KVM',
+    'iaas_hypervisor_version': '1.5.0',
+    'iaas_capabilities': ('cloud.managementSystem','cloud.vm.uploadImage'),
+}
 
-provider['iaas_endpoints'] = ({'endpoint_url':'https://prisma-cloud.ba.infn.it:8787','endpoint_interface':interface['IaaS_api'],'service_type_name':provider['iaas_middleware'],'service_type_version':provider['iaas_middleware_version'],'service_type_developer':provider['iaas_middleware_developer'],'interface_version':interface['IaaS_api_version'],'endpoint_technology':interface['IaaS_api_endpoint_technology'],'auth_method':interface['IaaS_api_authorization_method']},)
-
-provider['os_tpl'] = ({'image_name':'SL64-x86_64','image_version':'1.0','marketplace_id':'http://appdb.egi.eu/store/vm/image/2c24de6c-e385-49f1-b64f-f9ff35e70f43:9/xml','occi_id':'os#ef13c0be-4de6-428f-ad5b-8f32b31a54a1','os_family':'linux','os_name':'SL','os_version':'6.4','platform':'amd64'},
-		      {'image_name':'ubuntu-precise-server-amd64','image_version':'1.0','marketplace_id':'http://appdb.egi.eu/store/vm/image/703157c0-e509-44c8-8371-58beb44d80d6:8/xml','occi_id':'os#c0a2f9e0-081a-419c-b9a5-8cb03b1decb5','os_family':'linux','os_name':'Ubuntu','os_version':'12.04','platform':'amd64'},
-		      {'image_name':'CernVM3','image_version':'3.1.1.7','marketplace_id':'http://appdb.egi.eu/store/vm/image/dfb2f33e-ba3f-4c5a-a387-6257e8558ba1:24/xml','occi_id':'os#5364f77a-e1cb-4a6c-862e-96dc79c4ef67','os_family':'linux','os_name':'SL','os_version':'6.4','platform':'amd64'},)
-
-provider['resource_tpl'] = ({'occi_id':'resource#tiny-with-disk','memory':'512','cpu':'1','platform':'amd64','network':'public'},
-                          {'occi_id':'resource#small','memory':'1024','cpu':'1','platform':'amd64','network':'public'},
-                          {'occi_id':'resource#medium','memory':'4096','cpu':'2','platform':'amd64','network':'public'},
-                          {'occi_id':'resource#large','memory':'8196','cpu':'4','platform':'amd64','network':'public'},
-                          {'occi_id':'resource#extra_large','memory':'16384','cpu':'8','platform':'amd64','network':'public'},
+provider['iaas_endpoints'] = (
+    {
+        'endpoint_url':'https://prisma-cloud.ba.infn.it:8787',
+        'endpoint_interface': interface['IaaS_api'],
+        'service_type_name': provider['iaas_middleware'],
+        'service_type_version': provider['iaas_middleware_version'],
+        'service_type_developer': provider['iaas_middleware_developer'],
+        'interface_version': interface['IaaS_api_version'],
+        'endpoint_technology': interface['IaaS_api_endpoint_technology'],
+        'auth_method': interface['IaaS_api_authorization_method']
+    },
 )
+
+
+provider['os_tpl'] = (
+    {
+        'image_name': 'SL64-x86_64',
+        'image_version': '1.0',
+        'marketplace_id': 'http://appdb.egi.eu/store/vm/image/2c24de6c-e385-49f1-b64f-f9ff35e70f43:9/xml',
+        'occi_id': 'os#ef13c0be-4de6-428f-ad5b-8f32b31a54a1',
+        'os_family': 'linux',
+        'os_name': 'SL',
+        'os_version': '6.4',
+        'platform': 'amd64'
+    },
+    {
+        'image_name': 'ubuntu-precise-server-amd64',
+        'image_version': '1.0',
+        'marketplace_id': 'http://appdb.egi.eu/store/vm/image/703157c0-e509-44c8-8371-58beb44d80d6:8/xml',
+        'occi_id': 'os#c0a2f9e0-081a-419c-b9a5-8cb03b1decb5',
+        'os_family': 'linux',
+        'os_name': 'Ubuntu',
+        'os_version': '12.04',
+        'platform': 'amd64'
+    },
+    {
+        'image_name': 'CernVM3',
+        'image_version': '3.1.1.7',
+        'marketplace_id': 'http://appdb.egi.eu/store/vm/image/dfb2f33e-ba3f-4c5a-a387-6257e8558ba1:24/xml',
+        'occi_id': 'os#5364f77a-e1cb-4a6c-862e-96dc79c4ef67',
+        'os_family': 'linux',
+        'os_name': 'SL',
+        'os_version': '6.4',
+        'platform': 'amd64'
+    },
+)
+
+provider['resource_tpl'] = (
+    {
+        'occi_id': 'resource#tiny-with-disk',
+        'memory': '512',
+        'cpu': '1',
+        'platform': 'amd64',
+        'network': 'public'
+    },
+    {
+        'occi_id': 'resource#small',
+        'memory': '1024',
+        'cpu': '1',
+        'platform': 'amd64',
+        'network': 'public'
+    },
+    {
+        'occi_id': 'resource#medium',
+        'memory': '4096',
+        'cpu': '2',
+        'platform': 'amd64',
+        'network': 'public'
+    },
+    {
+        'occi_id': 'resource#large',
+        'memory': '8196',
+        'cpu': '4',
+        'platform': 'amd64',
+        'network': 'public'},
+    {
+        'occi_id': 'resource#extra_large',
+        'memory': '16384',
+        'cpu': '8',
+        'platform': 'amd64',
+        'network': 'public'
+    },
+)
+
 #STorage-as-a-Service
 provider['staas_middleware'] = 'OpenStack Swift' #e.g. CDMI Proxy. Leave empty for no storage service
 provider['staas_middleware_version'] = 'havana' #CLOUD_MW version, e.g. 3.0
 provider['staas_middleware_developer'] = 'OpenStack' #CLOUD_MW version, e.g. 3.0
 provider['staas_capabilities'] = 'cloud.data.upload' # Capabilities must be agreed with the FedClouds task force
 
-provider['staas_endpoints'] = ({'endpoint_url':'https://prisma-swift.ba.infn.it:8080','endpoint_interface':interface['STaaS_api'],'service_type_name':provider['staas_middleware'],'service_type_version':provider['staas_middleware_version'],'service_type_developer':provider['staas_middleware_developer'],'interface_version':interface['STaaS_api_version'],'endpoint_technology':interface['STaaS_api_endpoint_technology'],'auth_method':interface['STaaS_api_authorization_method']},)
+provider['staas_endpoints'] = (
+    {
+        'endpoint_url': 'https://prisma-swift.ba.infn.it:8080',
+        'endpoint_interface': interface['STaaS_api'],
+        'service_type_name': provider['staas_middleware'],
+        'service_type_version': provider['staas_middleware_version'],
+        'service_type_developer': provider['staas_middleware_developer'],
+        'interface_version': interface['STaaS_api_version'],
+        'endpoint_technology': interface['STaaS_api_endpoint_technology'],
+        'auth_method': interface['STaaS_api_authorization_method']
+    },
+)
 
 ######################
 # Main 				 #
