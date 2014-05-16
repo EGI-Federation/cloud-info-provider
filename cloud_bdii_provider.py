5b688532eabdd49bfe5d5958fde4198d98de61d4
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-05-16 10:38:29 +0200

    Add an OpenStack provider

--- cloud_bdii_provider.py
+++ cloud_bdii_provider.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import argparse
+import os
+import sys
+
 interface = {
     'IaaS_api': 'OCCI',
     'IaaS_api_version': '1.1',
@@ -199,6 +203,10 @@ class IaaSBDII(BaseBDII):
                                                 extra=ex_env))
 
         for app_env in provider['os_tpl']:
+            app_env.setdefault("image_description",
+                               ("%(image_name)s version %(image_version)s on "
+                                "%(os_family)s %(os_name)s %(os_version)s "
+                                "%(platform)s" % app_env))
             output.append(self._format_template("application_environment",
                                                 extra=app_env))
 
@@ -226,7 +234,173 @@ class CloudBDII(BaseBDII):
         return "\n".join(output)
 
 
+class OpenStackProvider(object):
+    def __init__(self, opts):
+        try:
+            import novaclient.client
+        except ImportError:
+            print >> sys.stderr, "ERROR: Cannot import nova module."
+            sys.exit(1)
+
+        (os_username, os_password, os_tenant_name, os_tenant_id,
+            os_auth_url, cacert, insecure) = (
+                opts.os_username, opts.os_password,
+                opts.os_tenant_name, opts.os_tenant_id,
+                opts.os_auth_url, opts.os_cacert, opts.insecure)
+
+        if not os_username:
+            print >> sys.stderr, ("ERROR, You must provide a username "
+                                  "via either --os-username or "
+                                  "env[OS_USERNAME]")
+            sys.exit(1)
+
+        if not os_password:
+            print >> sys.stderr, ("ERROR, You must provide a password "
+                                  "via either --os-password or "
+                                  "env[OS_PASSWORD]")
+            sys.exit(1)
+
+        if not os_tenant_name and not os_tenant_id:
+            print >> sys.stderr, ("You must provide a tenant name "
+                                  "or tenant id via --os-tenant-name, "
+                                  "--os-tenant-id, env[OS_TENANT_NAME] "
+                                  "or env[OS_TENANT_ID]")
+            sys.exit(1)
+
+        if not os_auth_url:
+            print >> sys.stderr, ("You must provide an auth url "
+                                  "via either --os-auth-url or "
+                                  "env[OS_AUTH_URL] ")
+            sys.exit(1)
+
+        client_cls = novaclient.client.get_client_class("2")
+        self.api = client_cls(os_username,
+                              os_password,
+                              os_tenant_name,
+                              tenant_id=os_tenant_id,
+                              auth_url=os_auth_url,
+                              insecure=insecure,
+                              cacert=cacert)
+
+        self.api.authenticate()
+
+    def get_images(self):
+        images = []
+
+        template = {
+            'image_name': None,
+            'image description': None,
+            'image_version': None,
+            'marketplace_id': None,
+            'occi_id': None,
+            'os_family': None,
+            'os_name': None,
+            'os_version': None,
+            'platform': None,
+        }
+
+        for image in self.api.images.list(detailed=True):
+            aux = template.copy()
+            aux.update({'image_name': image.name,
+                        'occi_id': 'os#%s' % image.id,
+                        'image_description': image.name,
+            })
+            image.metadata.pop("image_name", None)
+            image.metadata.pop("occi_id", None)
+            aux.update(image.metadata)
+            images.append(aux)
+        return images
+
+    @staticmethod
+    def populate_parser(parser):
+        parser.add_argument('--os-username',
+            metavar='<auth-user-name>',
+            default=env('OS_USERNAME', 'NOVA_USERNAME'),
+            help='Defaults to env[OS_USERNAME].')
+
+        parser.add_argument('--os-password',
+            metavar='<auth-password>',
+            default=env('OS_PASSWORD', 'NOVA_PASSWORD'),
+            help='Defaults to env[OS_PASSWORD].')
+
+        parser.add_argument('--os-tenant-name',
+            metavar='<auth-tenant-name>',
+            default=env('OS_TENANT_NAME', 'NOVA_PROJECT_ID'),
+            help='Defaults to env[OS_TENANT_NAME].')
+
+        parser.add_argument('--os-tenant-id',
+            metavar='<auth-tenant-id>',
+            default=env('OS_TENANT_ID'),
+            help='Defaults to env[OS_TENANT_ID].')
+
+        parser.add_argument('--os-auth-url',
+            metavar='<auth-url>',
+            default=env('OS_AUTH_URL', 'NOVA_URL'),
+            help='Defaults to env[OS_AUTH_URL].')
+
+        parser.add_argument('--os-cacert',
+            metavar='<ca-certificate>',
+            default=env('OS_CACERT', default=None),
+            help='Specify a CA bundle file to use in '
+                 'verifying a TLS (https) server certificate. '
+                 'Defaults to env[OS_CACERT]')
+
+        parser.add_argument('--insecure',
+            default=env('NOVACLIENT_INSECURE', default=False),
+            action='store_true',
+            help="Explicitly allow novaclient to perform \"insecure\" "
+                 "SSL (https) requests. The server's certificate will "
+                 "not be verified against any certificate authorities. "
+                 "This option should be used with caution.")
+
+
+SUPPORTED_MIDDLEWARE = {
+    'OpenStack': OpenStackProvider,
+}
+
+
+def env(*args, **kwargs):
+    """
+    returns the first environment variable set
+    if none are non-empty, defaults to '' or keyword arg default
+    """
+    for arg in args:
+        value = os.environ.get(arg, None)
+        if value:
+            return value
+    return kwargs.get('default', '')
+
+
+def parse_args():
+    parser = parser = argparse.ArgumentParser(
+        description='Cloud BDII provider')
+
+    parser.add_argument('--middleware',
+        metavar='MIDDLEWARE',
+        choices=SUPPORTED_MIDDLEWARE,
+        default=None,
+        help=('Middleware used. Only the following middlewares are '
+              'supported: %s. If you do not specify anything, static '
+              'values will be used.' % SUPPORTED_MIDDLEWARE.keys()))
+
+    for provider_name, provider in SUPPORTED_MIDDLEWARE.items():
+        group = parser.add_argument_group("%s provider options" %
+                                          provider_name)
+        provider.populate_parser(group)
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+
+    if args.middleware:
+        dynamic_provider = SUPPORTED_MIDDLEWARE[args.middleware](args)
+        images = dynamic_provider.get_images()
+        if images:
+            provider["os_tpl"] = images
+#        import pprint
+#        pprint.pprint(images)
     bdii = CloudBDII(provider)
     print bdii.render()
 
