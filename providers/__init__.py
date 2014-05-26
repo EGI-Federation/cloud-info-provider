5c9dc13d0563cfa44bf3600f74a2b23c4806ae3d
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-05-26 13:14:11 +0200

    Add some methods to the base class

--- providers/__init__.py
+++ providers/__init__.py
@@ -2,6 +2,21 @@ class BaseProvider(object):
     def __init__(self, opts):
         self.opts = opts
 
+    def get_site_info(self):
+        return {}
+
+    def get_images(self):
+        return []
+
+    def get_templates(self):
+        return []
+
+    def get_iaas_endpoints(self):
+        return []
+
+    def get_staas_endpoints(self):
+        return []
+
     @staticmethod
     def populate_parser(parser):
         """Populate the argparser 'parser' with the needed options."""
