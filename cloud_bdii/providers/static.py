f93dc433c5c6705c7aee924f6f05590d80755f69
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-05-27 12:21:32 +0200

    Use defaults for static providers and DRY

--- cloud_bdii/providers/static.py
+++ cloud_bdii/providers/static.py
@@ -1,3 +1,4 @@
+import copy
 import sys
 
 import yaml
@@ -84,119 +85,85 @@ class StaticProvider(providers.BaseProvider):
     def __init__(self, *args):
         super(StaticProvider, self).__init__(*args)
 
-        # FIXME(aloga): this is hardcoded
         self._load_yaml(self.opts.yaml_file)
-
-        self.site_info = {}
-        self.compute_endpoints = {}
-        self.storage_endpoints = {}
-        self.flavors = {}
-        self.images = {}
-
-        self._load_site_info()
-        self._load_compute_endpoint_info()
-
-        self._load_storage_endpoint_info()
 
     def _load_yaml(self, yaml_file):
         with open(yaml_file, "r") as f:
             self.yaml = yaml.safe_load(f)
 
-    def _format_fields_with_prefix(self, fields, prefix, ydata):
-        if ydata is None:
-            ydata = self.yaml
+    def _get_fields_and_prefix(self, fields, prefix, data, defaults={}):
+        if data is None:
+            data = self.yaml
 
         ret = {}
+        d = copy.deepcopy(defaults)
+        d.update(data)
+
         for field in fields:
-            if field not in ydata:
+            if field not in d:
                 print >> sys.stderr, ('ERROR: missing field %s on '
                                       '"%s" section' % (field, prefix))
                 sys.exit(1)
-
-            ret["%s%s" % (prefix, field)] = ydata[field]
+            ret["%s%s" % (prefix, field)] = d[field]
         return ret
 
-    def _load_site_info(self):
+    def _get_endpoints(self, type_, global_fields, endpoint_fields):
+        if type_ not in self.yaml:
+            return {}
+
+        endpoints = {"endpoints": {}}
+
+        data = copy.deepcopy(self.yaml[type_])
+        r = self._get_fields_and_prefix(global_fields, "%s_" % type_, data)
+        endpoints.update(r)
+
+        if "endpoints" in data:
+            defaults = data.pop("defaults", {})
+            for e, e_data in data["endpoints"].iteritems():
+                r = self._get_fields_and_prefix(endpoint_fields,
+                                                "%s_" % type_,
+                                                e_data,
+                                                defaults=defaults)
+                r["endpoint_url"] = e
+                endpoints["endpoints"][e] = r
+        return endpoints
+
+    def get_site_info(self):
+        data = self.yaml["site"].copy()
+
         fields = ("name", "production_level")
-        r = self._format_fields_with_prefix(fields, "site_",
-                                            self.yaml["site"])
-        self.site_info.update(r)
+        site_info = self._get_fields_and_prefix(fields, "site_", data)
 
         if self.opts.full_bdii_ldif:
             fields = ('url', 'ngi', 'country', 'latitude', 'longitude',
                       'general_contact', 'sysadmin_contact',
                       'security_contact', 'user_support_contact',
                       'bdii_host', 'bdii_port')
-            r = self._format_fields_with_prefix(fields, "site_",
-                                                self.yaml["site"])
-            self.site_info.update(r)
+            r = self._get_fields_and_prefix(fields, "site_", data)
+            site_info.update(r)
 
-    def _load_storage_endpoint_info(self):
-        if "storage" not in self.yaml:
-            return
-
-        self.storage_endpoints["endpoints"] = {}
-
-        fields = ('total_storage', 'capabilities', 'middleware',
-                  'middleware_version', 'middleware_developer')
-        r = self._format_fields_with_prefix(fields, "storage_",
-                                            self.yaml["storage"])
-        self.storage_endpoints.update(r)
-
-        if "endpoints" in self.yaml["storage"]:
-            for e, e_data in self.yaml["storage"]["endpoints"].iteritems():
-                fields = ('api_type', 'api_version',
-                          'api_endpoint_technology', 'api_authn_method')
-                r = self._format_fields_with_prefix(fields,
-                                                    "storage_",
-                                                    e_data)
-                r["endpoint_url"] = e
-                self.storage_endpoints["endpoints"][e] = r
-
-    def _load_compute_endpoint_info(self):
-        if "compute" not in self.yaml:
-            return
-
-        self.compute_endpoints["endpoints"] = {}
-
-        fields = ('total_ram','total_cores', 'capabilities', 'hypervisor',
-                  'hypervisor_version', 'middleware', 'middleware_version',
-                  'middleware_developer')
-        r = self._format_fields_with_prefix(fields,
-                                            "compute_",
-                                            self.yaml["compute"])
-        self.compute_endpoints.update(r)
-
-        if "endpoints" in self.yaml["compute"]:
-            for e, e_data in self.yaml["compute"]["endpoints"].iteritems():
-                fields = ('api_type', 'api_version', 'api_endpoint_technology',
-                          'api_authn_method')
-                r = self._format_fields_with_prefix(fields,
-                                                    "compute_",
-                                                    e_data)
-                r["endpoint_url"] = e
-                self.compute_endpoints["endpoints"][e] = r
-
-        self._load_flavors()
-        self._load_images()
-
-    def _load_flavors(self):
-        self.flavors = static_info.get("resource_tpl", [])
-
-    def _load_images(self):
-        self.images = static_info.get("os_tpl", [])
-
-    def get_site_info(self):
-        return self.site_info
+        return site_info
 
     def get_images(self):
-        return self.images
+        return static_info.get("os_tpl", [])
 
     def get_templates(self):
-        return self.flavors
+        return static_info.get("resource_tpl", [])
 
     def get_compute_endpoints(self):
-        return self.compute_endpoints
+        global_fields = ('total_ram','total_cores', 'capabilities', 'hypervisor',
+                         'hypervisor_version', 'middleware', 'middleware_version',
+                         'middleware_developer')
+        endpoint_fields = ('api_type', 'api_version', 'api_endpoint_technology',
+                           'api_authn_method')
+        endpoints = self._get_endpoints("compute", global_fields, endpoint_fields)
+        return endpoints
 
     def get_storage_endpoints(self):
-        return self.storage_endpoints
+        global_fields = ('total_storage', 'capabilities', 'middleware',
+                         'middleware_version', 'middleware_developer')
+        endpoint_fields = ('api_type', 'api_version', 'api_endpoint_technology',
+                           'api_authn_method')
+
+        endpoints = self._get_endpoints("storage", global_fields, endpoint_fields)
+        return endpoints
