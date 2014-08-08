1c478532fac5bb947620afd2b8a217a8553dfac6
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-08-08 11:19:02 +0200

    Separate data from tests

--- cloud_bdii/tests/test_static.py
+++ cloud_bdii/tests/test_static.py
@@ -2,6 +2,9 @@ import unittest
 import os.path
 
 from cloud_bdii.providers import static as static_provider
+from cloud_bdii.tests import data
+
+DATA = data.DATA
 
 
 class StaticProviderTest(unittest.TestCase):
@@ -52,11 +55,11 @@ class StaticProviderTest(unittest.TestCase):
                 {'foo_bar': 1, 'foo_foobar': 'barfoo', 'foo_baz': 2}
             ),
         )
-        for fields, prefix, data, yaml, defaults, expected in cases:
+        for fields, prefix, indata, yaml, defaults, expected in cases:
             self.provider.yaml = yaml
             ret = self.provider._get_fields_and_prefix(fields,
                                                        prefix,
-                                                       data,
+                                                       indata,
                                                        defaults=defaults)
             self.assertEqual(expected, ret)
 
@@ -185,110 +188,25 @@ class StaticProviderTest(unittest.TestCase):
         )
 
     def test_get_storage_endpoints(self):
-        expected = {
-            'endpoints': {
-                'https://storage-service01.example.org:8080': {
-                    'storage_api_authn_method': 'X509-VOMS',
-                    'storage_api_endpoint_technology': 'REST',
-                    'storage_api_type': 'CDMI',
-                    'storage_api_version': '1.0.1'
-                },
-                'https://storage-service02.example.org:8080': {
-                    'storage_api_authn_method': 'X509-VOMS',
-                    'storage_api_endpoint_technology': 'REST',
-                    'storage_api_type': 'CDMI',
-                    'storage_api_version': '1.0.1'
-                }
-            },
-            'storage_capabilities': ['cloud.data.upload'],
-            'storage_middleware': 'A Middleware',
-            'storage_middleware_developer': 'Middleware Developer',
-            'storage_middleware_version': 'v1.0',
-            'storage_total_storage': 0,
-        }
+        expected = DATA.storage_endpoints
         self.assertEqual(expected, self.provider.get_storage_endpoints())
 
     def test_get_compute_endpoints(self):
-        expected = {
-            'compute_capabilities': ['cloud.managementSystem',
-                                     'cloud.vm.uploadImage'],
-            'compute_hypervisor': 'Foo Hypervisor',
-            'compute_hypervisor_version': '0.0.0',
-            'compute_middleware': 'A Middleware',
-            'compute_middleware_developer': 'Middleware Developer',
-            'compute_middleware_version': 'v1.0',
-            'compute_total_cores': 0,
-            'compute_total_ram': 0,
-            'endpoints': {
-                'https://cloud-service01.example.org:8787': {
-                    'compute_api_authn_method': 'X509-VOMS',
-                    'compute_api_endpoint_technology': 'REST',
-                    'compute_api_type': 'OCCI',
-                    'compute_api_version': 1.1
-                },
-                'https://cloud-service02.example.org:8787': {
-                    'compute_api_authn_method': 'X509',
-                    'compute_api_endpoint_technology': 'REST',
-                    'compute_api_type': 'OCCI',
-                    'compute_api_version': 1.1
-                },
-                'https://cloud-service03.example.org:8787': {
-                    'compute_api_authn_method': 'User/Password',
-                    'compute_api_endpoint_technology': 'REST',
-                    'compute_api_type': 'OCCI',
-                    'compute_api_version': 1.1
-                }
-            }
-        }
-
+        expected = DATA.compute_endpoints
         self.assertEqual(expected, self.provider.get_compute_endpoints())
 
     def test_get_site_info_no_full_bdii(self):
-        expected = {
-            'site_name': 'SITE_NAME',
-            'site_production_level': 'production',
-            'suffix': 'o=glue'
-        }
-
+        expected = DATA.site_info
         self.provider.opts.full_bdii_ldif = False
         self.assertEqual(expected, self.provider.get_site_info())
 
     def test_get_site_info_full_bdii(self):
-        expected = {
-            'site_bdii_host': 'site.bdii.example.org',
-            'site_bdii_port': 2170,
-            'site_country': 'ES',
-            'site_general_contact': 'general-support@example.org',
-            'site_latitude': 0.0,
-            'site_longitude': 0.0,
-            'site_name': 'SITE_NAME',
-            'site_ngi': 'NGI_FOO',
-            'site_production_level': 'production',
-            'site_security_contact': 'security-support@example.org',
-            'site_sysadmin_contact': 'support@example.org',
-            'site_url': 'http://site.url.example.org/',
-            'site_user_support_contact': 'user-support@example.org',
-            'suffix': 'GLUE2DomainID=SITE_NAME,o=glue'
-        }
-
+        expected = DATA.site_info_full
         self.provider.opts.full_bdii_ldif = True
         self.assertEqual(expected, self.provider.get_site_info())
 
     def test_get_images(self):
-        expected = {
-            'os#foobarid': {
-                'image_marketplace_id': (
-                    'http://url.to.marketplace.id.example.org/foo/bar'
-                ),
-                'image_name': 'Foo Image',
-                'image_os_family': 'linux',
-                'image_os_name': 'Cirros',
-                'image_os_version': 1.0,
-                'image_platform': 'amd64',
-                'image_version': 1.0
-            }
-        }
-
+        expected = DATA.compute_images
         self.assertEqual(expected, self.provider.get_images())
 
     def test_get_images_with_yaml(self):
@@ -343,31 +261,5 @@ class StaticProviderTest(unittest.TestCase):
         self.assertEqual(expected, self.provider.get_images())
 
     def test_get_templates(self):
-        expected = {
-            'resource#extra_large': {
-                'template_cpu': 8,
-                'template_memory': 16384,
-                'template_network': 'public',
-                'template_platform': 'amd64'
-            },
-            'resource#large': {
-                'template_cpu': 4,
-                'template_memory': 8196,
-                'template_network': 'public',
-                'template_platform': 'amd64'
-            },
-            'resource#medium': {
-                'template_cpu': 2,
-                'template_memory': 4096,
-                'template_network': 'public',
-                'template_platform': 'amd64'
-            },
-            'resource#small': {
-                'template_cpu': 1,
-                'template_memory': 1024,
-                'template_network': 'public',
-                'template_platform': 'amd64'
-            }
-        }
-
+        expected = DATA.compute_templates
         self.assertEqual(expected, self.provider.get_templates())
