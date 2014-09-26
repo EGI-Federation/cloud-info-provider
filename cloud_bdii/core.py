eac2855d03765fb668a87aefd1b02e328ac32e6e
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-09-26 13:36:05 +0200

    fix duplicated variable

--- cloud_bdii/core.py
+++ cloud_bdii/core.py
@@ -160,7 +160,7 @@ class CloudBDII(BaseBDII):
 
 
 def parse_opts():
-    parser = parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description='Cloud BDII provider',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         fromfile_prefix_chars='@',
