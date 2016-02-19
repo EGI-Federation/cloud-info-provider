539b643dcd202a0224abe4efa12e49eb2281c9ad
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2016-02-19 12:03:50 +0100

    pep8: whitespace

--- bin/cloud-info-provider-service.py
+++ bin/cloud-info-provider-service.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-import os, sys, inspect
+import inspect
+import os
+import sys
 
 # realpath() will make your script run, even if you symlink it :)
-cmd_folder = os.path.abspath(os.path.split(os.path.realpath(inspect.getfile( inspect.currentframe() )))[0])
+cmd_folder = os.path.abspath(os.path.split(
+    os.path.realpath(inspect.getfile(inspect.currentframe())))[0])
 cmd_folder = os.path.split(cmd_folder)[0]
 if cmd_folder not in sys.path:
-     sys.path.insert(0, cmd_folder)
+    sys.path.insert(0, cmd_folder)
 os.chdir(cmd_folder)
 
 import cloud_bdii.core
