59477780f0f6a91f8220b2db13a2933ed5fe8aed
Mamut <Mamut3D@ics.muni.cz>
2016-04-05 15:48:48 +0200

    Add nifty attribute dependency between templates and images

--- cloud_bdii/tests/test_opennebula.py
+++ cloud_bdii/tests/test_opennebula.py
@@ -11,9 +11,9 @@ from cloud_bdii.tests import data
 FAKES = data.ONE_FAKES
 
 
-class OpenNebulaBaseProviderOptionsTest(unittest.TestCase):
+class OpenNebulaProviderOptionsTest(unittest.TestCase):
     def setUp(self):
-        self.provider = opennebula.OpenNebulaBaseProvider
+        self.provider = opennebula.OpenNebulaProvider
 
     def test_populate_parser(self):
         parser = argparse.ArgumentParser()
@@ -41,19 +41,9 @@ class OpenNebulaBaseProviderOptionsTest(unittest.TestCase):
                               self.provider, o)
 
 
-class OpenNebulaProviderOptionsTest(OpenNebulaBaseProviderOptionsTest):
-    def setUp(self):
-        self.provider = opennebula.OpenNebulaProvider
-
-
-class OpenNebulaROCCIProviderOptionsTest(OpenNebulaBaseProviderOptionsTest):
-    def setUp(self):
-        self.provider = opennebula.OpenNebulaROCCIProvider
-
-
-class OpenNebulaBaseProviderTest(unittest.TestCase):
+class OpenNebulaProviderTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        super(OpenNebulaBaseProviderTest, self).__init__(*args, **kwargs)
+        super(OpenNebulaProviderTest, self).__init__(*args, **kwargs)
         self.expected_images = {
             '80': {
                 'image_marketplace_id': (
@@ -64,7 +54,7 @@ class OpenNebulaBaseProviderTest(unittest.TestCase):
                 'image_platform': 'amd64',
                 'image_os_version': None,
                 'image_name': 'CERNVM-3.3.0-40GB@fedcloud-dukan',
-                'image_id': 'os_tpl#80',
+                'image_id': 'os_tpl#uuid_cernvm_3_3_0_40gb_fedcloud_dukan_80',
                 'image_description': (
                     'This version of CERNVM has been modified by EGI with '
                     'the followign changes - default OS extended to 40GB '
@@ -81,67 +71,12 @@ class OpenNebulaBaseProviderTest(unittest.TestCase):
                 'image_platform': 'amd64',
                 'image_os_version': None,
                 'image_name': 'Ubuntu-Server-14.04-LTS-ht-xxl@fedcloud-dukan',
-                'image_id': 'os_tpl#86',
+                'image_id': 'os_tpl#uuid_ubuntu_server_14_04_lts_ht_xxl_fedcloud_dukan_86',
                 'image_description': None,
                 'image_os_name': None,
                 'image_os_family': None},
         }
-        self.provider_class = opennebula.OpenNebulaBaseProvider
-
-    def setUp(self):
-        class FakeProvider(self.provider_class):
-            def __init__(self, opts):
-                self.on_auth = None
-                self.on_rpcxml_endpoint = "http://foo.bar.com/"
-                self.api = mock.Mock()
-                self.static = mock.Mock()
-                self.static.get_image_defaults.return_value = {}
-                self.opts = opts
-
-        class Opts(object):
-            on_auth = 'foo'
-            on_rpcxml_endpoint = 'bar'
-            vmcatcher_images = False
-
-        self.provider = FakeProvider(Opts())
-
-    @mock.patch('urllib2.urlopen')
-    def test_get_images(self, mock_open):
-        resp = mock.Mock()
-        resp.read.side_effect = [FAKES.templatepool, FAKES.imagepool]
-        mock_open.return_value = resp
-        self.assertDictEqual(self.expected_images,
-                             self.provider.get_images())
-
-    @mock.patch('urllib2.urlopen')
-    def test_get_filtered_images(self, mock_open):
-        resp = mock.Mock()
-        resp.read.side_effect = [FAKES.templatepool, FAKES.imagepool]
-        mock_open.return_value = resp
-        self.provider.opts.vmcatcher_images = True
-        filtered_images = {k: v for (k, v) in self.expected_images.items()
-                           if v.get('image_marketplace_id')}
-        self.assertDictEqual(filtered_images,
-                             self.provider.get_images())
-
-
-class OpenNebulaProviderTest(OpenNebulaBaseProviderTest):
-    def __init__(self, *args, **kwargs):
-        super(OpenNebulaProviderTest, self).__init__(*args, **kwargs)
         self.provider_class = opennebula.OpenNebulaProvider
-        self.maxDiff = None
-
-
-class OpenNebulaROCCIProviderTest(OpenNebulaBaseProviderTest):
-    def __init__(self, *args, **kwargs):
-        super(OpenNebulaROCCIProviderTest, self).__init__(*args, **kwargs)
-        self.provider_class = opennebula.OpenNebulaROCCIProvider
-        self.expected_images['80']['image_id'] = (
-            'os_tpl#uuid_cernvm_3_3_0_40gb_fedcloud_dukan_80'
-        )
-        self.expected_images['86']['image_id'] = (
-            'os_tpl#uuid_ubuntu_server_14_04_lts_ht_xxl_fedcloud_dukan_86'
-        )
 
     def setUp(self):
         class FakeProvider(self.provider_class):
@@ -161,12 +96,31 @@ class OpenNebulaROCCIProviderTest(OpenNebulaBaseProviderTest):
             vmcatcher_images = False
 
         self.provider = FakeProvider(Opts())
+    
+    @mock.patch('xmlrpccall')
+    def test_get_images(self, mock_open):
+        resp = mock.Mock()
+        resp.read.side_effect = [0,[FAKES.templatepool, FAKES.imagepool],0]
+        mock_open.return_value = resp
+        self.assertDictEqual(self.expected_images,
+                             self.provider.get_images())
+
+    @mock.patch('xmlrpccall')
+    def test_get_filtered_images(self, mock_open):
+        resp = mock.Mock()
+        resp.read.side_effect = [0,[FAKES.templatepool, FAKES.imagepool],0]
+        mock_open.return_value = resp
+        self.provider.opts.vmcatcher_images = True
+        filtered_images = {k: v for (k, v) in self.expected_images.items()
+                           if v.get('image_marketplace_id')}
+        self.assertDictEqual(filtered_images,
+                             self.provider.get_images())
 
     def test_templates_missing(self):
         fake_dir = uuid.uuid4().hex
         self.provider.opts.template_dir = fake_dir
         self.assertRaises(OSError, self.provider.get_templates)
 
-    def test_load_templates(self):
-        # TBD
+    def test_load_templates(self): 
+	# TBD
         pass
