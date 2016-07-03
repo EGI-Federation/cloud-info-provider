77819445f6f36620a57ffe67369b031d86b62285
Baptiste Grenier <baptiste.grenier@egi.eu>
2016-07-03 22:35:43 +0000

    core: templates_files was already created

--- cloud_info/core.py
+++ cloud_info/core.py
@@ -33,7 +33,6 @@ class BaseBDII(object):
         self.static_provider = SUPPORTED_MIDDLEWARE['static'](opts)
 
     def load_templates(self):
-        self.templates_files = {}
         for tpl in self.templates:
             template_extension = self.opts.template_extension
             template_file = os.path.join(self.opts.template_dir,
