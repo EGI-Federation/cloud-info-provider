043f6e15d10c39e7f25b453d681554f22118a802
Baptiste Grenier <baptiste@bapt.name>
2017-03-14 15:18:59 +0100

    core.py: fix style-related errors.
    (cherry picked from commit cf744d6e8c9a9d0539045be790f9b54bb045a437)

--- cloud_info/core.py
+++ cloud_info/core.py
@@ -39,7 +39,7 @@ class BaseBDII(object):
                                          '%s.%s' % (tpl, template_extension))
             self.templates_files[tpl] = template_file
 
-    def _get_info_from_providers(self, method, provider_opts = None):
+    def _get_info_from_providers(self, method, provider_opts=None):
         # XXX Temporarily update dynamic provider parameters
         # XXX Required to be able to pass a custom project to the provider
         # XXX to retrieve project-specific templates and images
