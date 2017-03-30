d8f7598f54d0848030625f5d9c5a11bf4da456a7
Baptiste Grenier <baptiste@bapt.name>
2017-03-30 10:58:04 +0200

    openstack provider: make insecure requests to OCCI endpoint if asked.

--- cloud_info/providers/openstack.py
+++ cloud_info/providers/openstack.py
@@ -248,7 +248,11 @@ class OpenStackProvider(providers.BaseProvider):
             try:
                 headers = {'X-Auth-token': self.auth_token}
                 request_url = "%s/-/" % endpoint_url
-                r = requests.get(request_url, headers=headers)
+                if self.insecure:
+                    verify = False
+                else:
+                    verify = self.os_cacert
+                r = requests.get(request_url, headers=headers, verify=verify)
                 if r.status_code == requests.codes.ok:
                     header_server = r.headers['Server']
                     e_middleware_version = re.search(r'ooi/([0-9.]+)',
