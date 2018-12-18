c0be7e8f2963b86faf54adb807b8ced2bd4f631a
Enol Fernandez <enol.fernandez@egi.eu>
2018-12-18 13:38:26 +0000

    Use defaults in shares

--- cloud_info_provider/providers/static.py
+++ cloud_info_provider/providers/static.py
@@ -137,6 +137,9 @@ class StaticProvider(providers.BaseProvider):
                                 None,
                                 fields,
                                 prefix='')
+        for vo, share in shares['shares'].items():
+            if not share['membership']:
+                share['membership'] = ['VO:%s' % vo]
         return shares['shares']
 
     def get_compute_quotas(self, **kwargs):
