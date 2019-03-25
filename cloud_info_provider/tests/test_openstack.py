87a67707a84827ae85bdf8c8713f944344b5e552
Pablo Orviz <orviz@ifca.unican.es>
2019-03-25 12:21:52 +0100

    Fix: rename openstack provider opts for infiniband

--- cloud_info_provider/tests/test_openstack.py
+++ cloud_info_provider/tests/test_openstack.py
@@ -65,8 +65,8 @@ class OpenStackProviderTest(base.TestCase):
                 self._rescope_project = mock.Mock()
                 self.all_images = False
                 self.opts = mock.Mock()
-                self.opts.extra_specs_infiniband_key = 'infiniband'
-                self.opts.extra_specs_infiniband_value = 'true'
+                self.opts.property_infiniband = 'infiniband'
+                self.opts.property_infiniband_value = 'true'
 
         self.provider = FakeProvider(None)
 
@@ -682,8 +682,8 @@ class OoiProviderTest(OpenStackProviderTest):
                 self._rescope_project = mock.Mock()
                 self.all_images = False
                 self.opts = mock.Mock()
-                self.opts.extra_specs_infiniband_key = 'infiniband'
-                self.opts.extra_specs_infiniband_value = 'true'
+                self.opts.property_infiniband = 'infiniband'
+                self.opts.property_infiniband_value = 'true'
 
         self.provider = FakeProvider(None)
 
