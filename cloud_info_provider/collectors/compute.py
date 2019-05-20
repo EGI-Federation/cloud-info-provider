85a94f145d3bf8a6feb9c299bf614218b7b3d46b
Pablo Orviz <orviz@ifca.unican.es>
2019-05-20 17:03:35 +0200

    Move endpoints gathering outside the shares loop (collector)

--- cloud_info_provider/collectors/compute.py
+++ cloud_info_provider/collectors/compute.py
@@ -16,47 +16,52 @@ class ComputeCollector(base.BaseCollector):
         # XXX Here it uses the "default" project from the CLI parameters
         site_info = self._get_info_from_providers('get_site_info')
 
+        endpoints = self._get_info_from_providers('get_compute_endpoints')
+        if not endpoints.get('endpoints'):
+            return {}
+
         # Get shares / projects and related images and templates
         shares = self._get_info_from_providers('get_compute_shares')
 
-        for share in shares.values():
-            kwargs = share.copy()
+        if shares:
+            for share in shares.values():
+                kwargs = share.copy()
 
-            endpoints = self._get_info_from_providers('get_compute_endpoints',
-                                                      **kwargs)
-            if not endpoints.get('endpoints'):
-                return {}
+                #endpoints = self._get_info_from_providers('get_compute_endpoints',
+                #                                          **kwargs)
+                #if not endpoints.get('endpoints'):
+                #    return {}
 
-            # Collect static information for endpoints
-            static_compute_info = dict(endpoints, **site_info)
-            static_compute_info.pop('endpoints')
+                # Collect static information for endpoints
+                static_compute_info = dict(endpoints, **site_info)
+                static_compute_info.pop('endpoints')
 
-            # Collect dynamic information
-            images = self._get_info_from_providers('get_images',
-                                                   **kwargs)
-            templates = self._get_info_from_providers('get_templates',
-                                                      **kwargs)
-            instances = self._get_info_from_providers('get_instances',
-                                                      **kwargs)
-            quotas = self._get_info_from_providers('get_compute_quotas',
-                                                   **kwargs)
+                # Collect dynamic information
+                images = self._get_info_from_providers('get_images',
+                                                       **kwargs)
+                templates = self._get_info_from_providers('get_templates',
+                                                          **kwargs)
+                instances = self._get_info_from_providers('get_instances',
+                                                          **kwargs)
+                quotas = self._get_info_from_providers('get_compute_quotas',
+                                                       **kwargs)
 
-            # Add same static information to endpoints, images and templates
-            for d in itertools.chain(endpoints['endpoints'].values(),
-                                     templates.values(),
-                                     images.values()):
-                d.update(static_compute_info)
+                # Add same static information to endpoints, images and templates
+                for d in itertools.chain(endpoints['endpoints'].values(),
+                                         templates.values(),
+                                         images.values()):
+                    d.update(static_compute_info)
 
-            share['images'] = images
-            share['templates'] = templates
-            share['instances'] = instances
-            share['endpoints'] = endpoints
-            share['quotas'] = quotas
+                share['images'] = images
+                share['templates'] = templates
+                share['instances'] = instances
+                share['endpoints'] = endpoints
+                share['quotas'] = quotas
 
-        # XXX Avoid creating a new list
-        endpoints = {endpoint_id: endpoint for share_id, share in
-                     shares.items() for endpoint_id,
-                     endpoint in share['endpoints'].items()}
+            # XXX Avoid creating a new list
+            endpoints = {endpoint_id: endpoint for share_id, share in
+                         shares.items() for endpoint_id,
+                         endpoint in share['endpoints'].items()}
 
         # XXX Avoid redoing what was done in the previous shares loop
         static_compute_info = dict(endpoints, **site_info)
