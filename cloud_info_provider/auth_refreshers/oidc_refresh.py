0013013bf0711b304a1bd0244269b849cb4ddfc8
Enol Fernandez <enol.fernandez@egi.eu>
2019-06-26 09:15:11 +0200

    Refactor the refresher to facilitate reuse

--- cloud_info_provider/auth_refreshers/oidc_refresh.py
+++ cloud_info_provider/auth_refreshers/oidc_refresh.py
@@ -5,30 +5,42 @@ from cloud_info_provider import exceptions
 
 
 class OidcRefreshToken(auth_refreshers.BaseRefresher):
-    def refresh(self, provider, **kwargs):
+    def _refresh_token(self, token_endpoint, client_id, client_secret,
+                       refresh_token, scopes):
         refresh_data = {
-            "client_id": self.opts.oidc_client_id,
-            "client_secret": self.opts.oidc_client_secret,
+            "client_id": client_id,
+            "client_secret": client_secret,
             "grant_type": "refresh_token",
-            "refresh_token": self.opts.oidc_refresh_token,
-            "scope": self.opts.oidc_scopes,
+            "refresh_token": refresh_token,
+            "scope": scopes,
         }
         try:
-            r = requests.post(self.opts.oidc_token_endpoint,
-                              auth=(self.opts.oidc_client_id,
-                                    self.opts.oidc_client_secret),
+            r = requests.post(token_endpoint,
+                              auth=(client_id,
+                                    client_secret),
                               data=refresh_data)
             if r.status_code != requests.codes.ok:
                 msg = "Unable to get token, request returned %s" % r.text
                 raise exceptions.RefresherException(msg)
-            # this requires some inner knowledge on the oidc auth of OpenStack
-            # and won't work for others, but I'm not sure if we can make
-            # this generic
-            provider.opts.oidcaccesstoken = r.json()["access_token"]
+            return r.json()["access_token"]
         except (ValueError,
                 KeyError,
                 requests.exceptions.RequestException) as e:
             raise exceptions.RefresherException('Unable to get token %s' % e)
+
+    def _update_provider(self, provider, token):
+        # this requires some inner knowledge on the oidc auth of OpenStack
+        # and won't work for others, but I'm not sure if we can make
+        # this generic
+        provider.opts.oidcaccesstoken = token
+
+    def refresh(self, provider, **kwargs):
+        token = self._refresh_token(self.opts.oidc_token_endpoint,
+                                    self.opts.oidc_client_id,
+                                    self.opts.oidc_client_secret,
+                                    self.opts.oidc_refresh_token,
+                                    self.opts.oidc_scopes)
+        self._update_provider(provider, token)
 
     @staticmethod
     def populate_parser(parser):
