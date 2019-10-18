3a3d6aa0d2c9d653e2f9a0eb8e26d99aab899378
Pablo Orviz <orviz@ifca.unican.es>
2019-10-18 11:28:32 +0000

    Get iam_organisation from static configuration

--- cloud_info_provider/providers/static.py
+++ cloud_info_provider/providers/static.py
@@ -144,7 +144,7 @@ class StaticProvider(providers.BaseProvider):
         fields = ('instance_max_cpu', 'instance_max_ram',
                   'instance_max_accelerators',
                   'auth', 'sla', 'network_info', 'default_network_type',
-                  'public_network_name', 'membership')
+                  'public_network_name', 'membership', 'iam_organisation')
         shares = self._get_what('compute',
                                 'shares',
                                 None,
