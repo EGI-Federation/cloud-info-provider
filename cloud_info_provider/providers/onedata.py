1e19558a1a20ecb3bda16493e9a988d464c41ae4
Pablo Orviz <orviz@ifca.unican.es>
2021-01-21 10:22:34 +0100

    Adapt to current style

--- cloud_info_provider/providers/onedata.py
+++ cloud_info_provider/providers/onedata.py
@@ -12,19 +12,19 @@ class OnedataProvider(base.BaseProvider):
         super(OnedataProvider, self).__init__(opts, **kwargs)
 
         if not opts.onezone_api_url:
-            msg = ('You must provide a OneData API '
-                   'endpoint via --onedata-endpoint (alternatively using '
-                   'the environment variable ONEZONE_API_URL)')
+            msg = ("You must provide a OneData API "
+                   "endpoint via --onedata-endpoint (alternatively using "
+                   "the environment variable ONEZONE_API_URL)")
             raise exceptions.OnedataProviderException(msg)
 
         self.onezone_api_url = opts.onezone_api_url
-        self.goc_service_type = 'eu.egi.cloud.storage-management.oneprovider'
+        self.goc_service_type = "eu.egi.cloud.storage-management.oneprovider"
 
         self.static = static.StaticProvider(opts)
 
         self.headers = {}
         if opts.oidc_token:
-            self.headers['X-Auth-Token'] = '%s: %s' % (opts.oidc_idp_prefix,
+            self.headers["X-Auth-Token"] = "%s: %s" % (opts.oidc_idp_prefix,
                                                        opts.oidc_token)
 
     def get_oneproviders_from_onezone(self):
@@ -33,19 +33,19 @@ class OnedataProvider(base.BaseProvider):
             if r.status_code == requests.codes.ok:
                 return r.json()
             else:
-                msg = 'Request failed: %s' % r.content
+                msg = "Request failed: %s" % r.content
                 raise exceptions.OnedataProviderException(msg)
-        _url = '/'.join([self.onezone_api_url, 'onezone/providers'])
+        _url = "/".join([self.onezone_api_url, "onezone/providers"])
         try:
-            oneprov_ids = _do_request(_url)['providers']
+            oneprov_ids = _do_request(_url)["providers"]
         except KeyError:
             oneprov_ids = []
         d = {}
         for oneprov_id in oneprov_ids:
-            _oneprov_url = '/'.join([_url, oneprov_id])
+            _oneprov_url = "/".join([_url, oneprov_id])
             try:
-                _domain = _do_request(_oneprov_url)['domain']
-                d[_domain] = {'id': oneprov_id}
+                _domain = _do_request(_oneprov_url)["domain"]
+                d[_domain] = {"id": oneprov_id}
             except KeyError:
                 raise exceptions.OnedataProviderException((
                     "Cannot get Oneprovider domains from Onezone"))
@@ -57,7 +57,7 @@ class OnedataProvider(base.BaseProvider):
         defaults = self.static.get_storage_endpoint_defaults(prefix=True)
         defaults_endpoint = self.static.get_storage_endpoints()
         try:
-            endp_data = defaults_endpoint.pop('endpoints')
+            endp_data = defaults_endpoint.pop("endpoints")
         except KeyError:
             raise exceptions.OnedataProviderException((
                 "Static configuration file does not contain"
@@ -68,36 +68,45 @@ class OnedataProvider(base.BaseProvider):
         for oneprov_domain, oneprov_data in endp_data.items():
             d_endp[oneprov_domain] = {}
             try:
-                oneprov_id = d_onezone[oneprov_domain]['id']
+                oneprov_id = d_onezone[oneprov_domain]["id"]
             except KeyError:
                 continue
-            d_endp[oneprov_domain]['onedata_id'] = oneprov_id
-            d_endp[oneprov_domain]['goc_service_type'] = self.goc_service_type
+            d_endp[oneprov_domain]["onedata_id"] = oneprov_id
+            d_endp[oneprov_domain]["goc_service_type"] = self.goc_service_type
             aux = oneprov_data.copy()
             aux.update(defaults_endpoint)
             d_endp[oneprov_domain].update(aux)
-        return {'endpoints': d_endp}
+        return {"endpoints": d_endp}
 
     @staticmethod
     def populate_parser(parser):
         parser.add_argument(
-            '--onezone-api-url',
-            metavar='<api-url>',
-            default=utils.env('ONEZONE_API_URL'),
-            help=('Specify OneData API endpoint with format: '
-                  'https://onezone.example.org/api/v3. '
-                  'Defaults to env[ONEZONE_API_URL]'))
+            "--onezone-api-url",
+            metavar="<api-url>",
+            default=utils.env("ONEZONE_API_URL"),
+            help=(
+                "Specify OneData API endpoint with format: "
+                "https://onezone.example.org/api/v3. "
+                "Defaults to env[ONEZONE_API_URL]"
+            ),
+        )
         parser.add_argument(
-            '--oidc-x-auth-token',
-            metavar='<x-auth-token>',
-            default=utils.env('ACCESS_BEARER_TOKEN'),
-            dest='oidc_token',
-            help=('Specify OIDC X-Auth token to use when '
-                  'authenticating with the API. Defaults '
-                  'to env[ACCESS_BEARER_TOKEN]'))
+            "--oidc-x-auth-token",
+            metavar="<x-auth-token>",
+            default=utils.env("ACCESS_BEARER_TOKEN"),
+            dest="oidc_token",
+            help=(
+                "Specify OIDC X-Auth token to use when "
+                "authenticating with the API. Defaults "
+                "to env[ACCESS_BEARER_TOKEN]"
+            ),
+        )
         parser.add_argument(
-            '--oidc-idp-prefix',
-            metavar='<idp-prefix>',
-            dest='oidc_idp_prefix',
-            help=('Specify OIDC X-Auth IdP prefix for the '
-                  'X-Auth token'))
+            "--oidc-idp-prefix",
+            metavar="<idp-prefix>",
+            dest="oidc_idp_prefix",
+            help=(
+                "Specify OIDC X-Auth IdP prefix for the "
+                "X-Auth token"
+            ),
+        )
