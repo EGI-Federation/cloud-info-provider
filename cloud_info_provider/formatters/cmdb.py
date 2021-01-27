57cac94ad512438b9b03b5e129cee8ece0c3f32e
Pablo Orviz <orviz@ifca.unican.es>
2021-01-27 13:51:23 +0100

    Rename CMDB templates so they are not considered as valid JSON files by the linter

--- cloud_info_provider/formatters/cmdb.py
+++ cloud_info_provider/formatters/cmdb.py
@@ -7,5 +7,5 @@ from cloud_info_provider.formatters import base
 
 class CMDB(base.BaseFormatter):
     def __init__(self):
-        self.template_extension = "cmdb.json"
+        self.template_extension = "cmdb.json.tmpl"
         self.templates = ["compute", "storage"]
