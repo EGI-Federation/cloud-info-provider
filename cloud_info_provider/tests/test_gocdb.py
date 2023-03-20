5775ad4b9064069001bbb396fc18a2ad5d0f3324
Enol Fernández <enol.fernandez@egi.eu>
2023-03-20 06:47:08 +0000

    Add support for endpoints in GOCDB (#234)
    * Add support for endpoints in GOCDB
    
    * Update changelog
    
    * Add missing library
    
    How this was not missing before?
    
    * Do not debug...
    
    * More test requirements
    
    * Add missing requirements
    
    * Update actions
    
    * Py 2.7 should be gone by now
    
    * Fix linting issues
    
    * lovely yaml
    
    * Do not use real sites names

--- cloud_info_provider/tests/test_gocdb.py
+++ cloud_info_provider/tests/test_gocdb.py
@@ -4,21 +4,55 @@ from cloud_info_provider.tests import base
 
 sample_goc_response = """<?xml version="1.0" encoding="UTF-8"?>
 <results>
-    <SERVICE_ENDPOINT PRIMARY_KEY="7513G0">
-        <PRIMARY_KEY>7513G0</PRIMARY_KEY>
-        <HOSTNAME>nova.cloud.ifca.es</HOSTNAME>
+    <SERVICE_ENDPOINT PRIMARY_KEY="1234G0">
+        <PRIMARY_KEY>1234G0</PRIMARY_KEY>
+        <HOSTNAME>simple.example.com</HOSTNAME>
         <SERVICE_TYPE>org.openstack.nova</SERVICE_TYPE>
         <CORE></CORE>
         <IN_PRODUCTION>Y</IN_PRODUCTION>
         <NODE_MONITORED>Y</NODE_MONITORED>
         <NOTIFICATIONS>Y</NOTIFICATIONS>
-        <SITENAME>IFCA-LCG2</SITENAME>
+        <SITENAME>FOO-BAR-SITE</SITENAME>
         <COUNTRY_NAME>Spain</COUNTRY_NAME>
         <COUNTRY_CODE>ES</COUNTRY_CODE>
         <ROC_NAME>NGI_IBERGRID</ROC_NAME>
-        <URL>https://keystone.ifca.es:5000/v2.0/?image=18d99a06-c3e5-4157-a0e3-37ec34bdfc24&amp;resource=m1.tiny</URL>
+        <URL>https://keystone.example.com:5000/v2.0/?image=18d99a06-c3e5-4157-a0e3-37ec34bdfc24&amp;resource=m1.tiny</URL>
         <ENDPOINTS/>
-  </SERVICE_ENDPOINT>'
+  </SERVICE_ENDPOINT>
+</results>"""
+
+sample_goc_ep_response = """<?xml version="1.0" encoding="UTF-8"?>
+<results>
+  <SERVICE_ENDPOINT PRIMARY_KEY="00000G0">
+    <PRIMARY_KEY>00000G0</PRIMARY_KEY>
+    <HOSTNAME>www.baz.example.com</HOSTNAME>
+    <GOCDB_PORTAL_URL>https://goc.egi.eu/portal/index.php?Page_Type=Service&amp;id=14541</GOCDB_PORTAL_URL>
+    <BETA>N</BETA>
+    <SERVICE_TYPE>org.openstack.nova</SERVICE_TYPE>
+    <CORE/>
+    <IN_PRODUCTION>Y</IN_PRODUCTION>
+    <NODE_MONITORED>Y</NODE_MONITORED>
+    <NOTIFICATIONS>Y</NOTIFICATIONS>
+    <SITENAME>BAR-FOO-SITE</SITENAME>
+    <COUNTRY_NAME>Ireland</COUNTRY_NAME>
+    <COUNTRY_CODE>IE</COUNTRY_CODE>
+    <ROC_NAME>NGI_IE</ROC_NAME>
+    <URL/>
+    <ENDPOINTS>
+      <ENDPOINT>
+        <ID>8282</ID>
+        <NAME>Name-it</NAME>
+        <EXTENSIONS/>
+        <URL>https://horizon.baz.example.com:5000/v3</URL>
+        <INTERFACENAME>org.openstack.nova</INTERFACENAME>
+        <ENDPOINT_MONITORED>Y</ENDPOINT_MONITORED>
+      </ENDPOINT>
+    </ENDPOINTS>
+    <SCOPES>
+      <SCOPE>EGI</SCOPE>
+    </SCOPES>
+    <EXTENSIONS/>
+  </SERVICE_ENDPOINT>
 </results>"""
 
 
@@ -88,10 +122,10 @@ class GOCDBTest(base.TestCase):
             r.status_code = 200
             r.text = sample_goc_response
             m_requests.return_value = r
-            expected = {"gocdb_id": "7513G0", "site_name": "IFCA-LCG2"}
+            expected = {"gocdb_id": "1234G0", "site_name": "FOO-BAR-SITE"}
             self.assertEqual(
                 expected,
-                gocdb.find_in_gocdb("https://keystone.ifca.es:5000/v2.0/", "bar"),
+                gocdb.find_in_gocdb("https://keystone.example.com:5000/v2.0/", "bar"),
             )
 
     def test_goc_found_similar_path(self):
@@ -100,8 +134,20 @@ class GOCDBTest(base.TestCase):
             r.status_code = 200
             r.text = sample_goc_response
             m_requests.return_value = r
-            expected = {"gocdb_id": "7513G0", "site_name": "IFCA-LCG2"}
+            expected = {"gocdb_id": "1234G0", "site_name": "FOO-BAR-SITE"}
             self.assertEqual(
                 expected,
-                gocdb.find_in_gocdb("https://keystone.ifca.es:5000/v2.0", "bar"),
+                gocdb.find_in_gocdb("https://keystone.example.com:5000/v2.0", "bar"),
+            )
+
+    def test_goc_multiple_endpoints(self):
+        with mock.patch("requests.get") as m_requests:
+            r = mock.MagicMock()
+            r.status_code = 200
+            r.text = sample_goc_ep_response
+            m_requests.return_value = r
+            expected = {"gocdb_id": "00000G0", "site_name": "BAR-FOO-SITE"}
+            self.assertEqual(
+                expected,
+                gocdb.find_in_gocdb("https://horizon.baz.example.com:5000/v3", "bar"),
             )
