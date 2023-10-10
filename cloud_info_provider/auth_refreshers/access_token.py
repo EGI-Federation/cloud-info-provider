68d2b634fb6a210d3084c2714def19f50b69dbec
Enol Fernandez <enol.fernandez@egi.eu>
2023-10-10 15:44:17 +0100

    Paint it black

--- cloud_info_provider/auth_refreshers/access_token.py
+++ cloud_info_provider/auth_refreshers/access_token.py
@@ -3,8 +3,8 @@ from cloud_info_provider import auth_refreshers, exceptions
 
 
 class AccessTokenRefresh(auth_refreshers.OidcBaseRefresher):
-    """Just uses the access token available at the auth configuration
-    """
+    """Just uses the access token available at the auth configuration"""
+
     def _update_provider(self, provider, token):
         # this requires some inner knowledge on the oidc auth of OpenStack
         # and won't work for others, but I'm not sure if we can make
